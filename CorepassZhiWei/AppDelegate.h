c8894c5d209ef71671f40875a2b3289cf5e4e6b2
杨松辉 <yangsonghui@yangsonghuidemacbook-air.local>
2016-03-22 11:44:45 +0800

    Initial Commit

---
+++ CorepassZhiWei/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CorepassZhiWei
+//
+//  Created by 杨松辉 on 16/3/22.
+//  Copyright © 2016年 YANG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CorepassZhiWei/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CorepassZhiWei
+//
+//  Created by 杨松辉 on 16/3/22.
+//  Copyright © 2016年 YANG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
